6ad914d631320eff779b4263c34eeb89b4fef603
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2023-12-10 14:40:11 +0700

    Initial commit

---
+++ src/main/java/com/alex/tgbotproject/TgbotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alex.tgbotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgbotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgbotProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alex/tgbotproject/controllers/CurrencyController.java
@@ -0,0 +1,21 @@
+package com.alex.tgbotproject.controllers;
+
+import com.alex.tgbotproject.dto.ValuteCursOnDate;
+import com.alex.tgbotproject.service.CentralRussianBankService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CurrencyController {
+
+    private final CentralRussianBankService centralRussianBankService;
+
+    @PostMapping("/getCurrencies")
+    public List<ValuteCursOnDate> getValuteCursOnDate() throws Exception {
+        return centralRussianBankService.getCurrenciesFromCbr();
+    }
+}
