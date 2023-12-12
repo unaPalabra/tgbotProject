16aa0755d0ce0ceee85762c616fe8d87251f6a70
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2023-12-12 20:32:55 +0700

    first

---
+++ src/test/java/com/alex/tgbotproject/TgbotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.tgbotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
